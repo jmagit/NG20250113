6e07018e38c8ac9b75b81f3a757f93173ab6a5cb
Javier <35864946+jmagit@users.noreply.github.com>
2025-01-21 11:03:23 +0100

    dashboard

--- curso/src/app/ejemplos/dashboard/dashboard.component.ts
+++ curso/src/app/ejemplos/dashboard/dashboard.component.ts
@@ -3,19 +3,19 @@ import { HomeComponent } from 'src/app/main/home/home.component';
 import { DemosComponent } from '../demos/demos.component';
 import GraficoSvgComponent from '../grafico-svg/grafico-svg.component';
 import { NotificationComponent } from 'src/app/main';
-import { CommonModule, NgComponentOutlet } from '@angular/common';
+import { NgComponentOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ NotificationComponent, CommonModule  ],
+  imports: [ NotificationComponent, NgComponentOutlet  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   menu = [
-    { texto: 'Demo', icono: 'fas fa-home', componente: DemosComponent },
-    { texto: 'Inicio', icono: 'fas fa-home', componente: HomeComponent },
-    { texto: 'Grafo', icono: 'fas fa-home', componente: GraficoSvgComponent },
+    { texto: 'demos', icono: 'fa-solid fa-person-chalkboard', componente: DemosComponent },
+    { texto: 'inicio', icono: 'fa-solid fa-house', componente: HomeComponent },
+    { texto: 'grafo', icono: 'fa-solid fa-image', componente: GraficoSvgComponent },
   ]
   actual = signal(0)
   cuerpo = computed(() => this.menu[this.actual()]?.componente)
