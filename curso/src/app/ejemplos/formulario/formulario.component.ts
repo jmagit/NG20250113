6160a129f6410662e40a52b9b882c7aacf746ab9
Javier <jmartin@grupoloyal.com>
2025-01-22 12:01:54 +0100

    formulario

--- curso/src/app/ejemplos/formulario/formulario.component.ts
+++ curso/src/app/ejemplos/formulario/formulario.component.ts
@@ -1,11 +1,41 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ErrorMessagePipe } from '@my/core';
+import { FormButtonsComponent } from 'src/app/common-component';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [ FormsModule, FormButtonsComponent, JsonPipe, ErrorMessagePipe, ],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  modo: 'add' | 'edit' = 'add'
+  elemento: any = {  }
 
+  add() {
+    this.elemento = {}
+    this.modo = 'add'
+  }
+  edit(key: number) {
+    this.elemento = { id: key, nombre: 'Pepito', apellidos: 'Grillo', email: 'pgrillo@example.com', edad: 99
+      , fecha: '2021-01-01', nif: '12345678z', activo: true }
+    this.modo = 'edit'
+  }
+  cancel() {
+
+  }
+
+  send() {
+    switch (this.modo) {
+      case 'add':
+        alert(`POST ${JSON.stringify(this.elemento)}` )
+        break
+      case 'edit':
+        alert(`PUT ${this.elemento.id} ${JSON.stringify(this.elemento)}` )
+        break
+    }
+  }
 }
