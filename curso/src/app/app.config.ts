dac14710042ac840cc09d29c8dba0f3a2335a84c
Javier <jmartin@grupoloyal.com>
2025-01-23 11:10:19 +0100

    Reactividad

--- curso/src/app/app.config.ts
+++ curso/src/app/app.config.ts
@@ -9,6 +9,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { ERROR_LEVEL, LoggerService } from '@my/core';
 import { environment } from 'src/environments/environment';
+import { provideHttpClient, withInterceptorsFromDi, withInterceptors } from '@angular/common/http';
+import { ajaxWaitInterceptor } from './main';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +19,7 @@ export const appConfig: ApplicationConfig = {
     // { provide: LoggerService, useValue: new LoggerService() },
     {provide: LOCALE_ID, useValue: 'es-ES'},
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi(), withInterceptors([ajaxWaitInterceptor])),
   ]
 };
--- curso/src/app/ejemplos/dashboard/dashboard.component.ts
+++ curso/src/app/ejemplos/dashboard/dashboard.component.ts
@@ -6,10 +6,11 @@ import { NotificationComponent } from 'src/app/main';
 import { NgComponentOutlet } from '@angular/common';
 import { CalculadoraComponent } from '../calculadora/calculadora.component';
 import { FormularioComponent } from '../formulario/formulario.component';
+import { LoginComponent, LoginFormComponent } from 'src/app/security';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ NotificationComponent, NgComponentOutlet  ],
+  imports: [ NotificationComponent, NgComponentOutlet, LoginComponent  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
@@ -20,6 +21,7 @@ export class DashboardComponent {
     { texto: 'demos', icono: 'fa-solid fa-person-chalkboard', componente: DemosComponent },
     { texto: 'calculadora', icono: 'fa-solid fa-calculator', componente: CalculadoraComponent },
     { texto: 'grafo', icono: 'fa-solid fa-image', componente: GraficoSvgComponent },
+    { texto: 'login', icono: 'fa-solid fa-image', componente: LoginFormComponent },
   ]
   actual = signal(0)
   cuerpo = computed(() => this.menu[this.actual()]?.componente)
